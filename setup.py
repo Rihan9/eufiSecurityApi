4f7fa23955efefde605598a610e7277705ff94b5
Rihan9 <aleatza@gmail.com>
2021-01-26 15:27:28 +0100

    addd initial setup, renamo folder properly

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="uefySecurityApi", # Replace with your own username
+    version="0.0.1",
+    author="Rihan",
+    author_email="aleatza@gmail.com",
+    description="Cover the http API for eufy security devices",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Rihan9/eufiSecurityApi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
