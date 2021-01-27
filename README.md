3f1a052c45822dfa83aff3c6d0c47d214ebae7bf
Rihan9 <aleatza@gmail.com>
2021-01-27 10:58:17 +0100

    setup for distribution

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Eufy unofficial API
 This library is able to connect on eufy security api on mysecurity.eufylife.com and expose methods and model to interact with it in Python.
 
+Install with:
+> pip install uefy-security-api-rihan==0.0.1
+
 TODO List:
 
 - [ ] Write decent readme. Sorry.
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="uefySecurityApi", # Replace with your own username
+    name="uefy-security-api-rihan", # Replace with your own username
     version="0.0.1",
     author="Rihan",
     author_email="aleatza@gmail.com",
@@ -13,10 +13,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Rihan9/eufiSecurityApi",
     packages=setuptools.find_packages(),
+    install_requires= ['requests', 'click', 'pytz', 'setuptools'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    license= 'MIT License'
 )
